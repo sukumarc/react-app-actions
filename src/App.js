2ff2695be3b4803c04b8c096a0f73a1d6d9d0164
Sukumar Chandrasekaran <csukumar29@gmail.com>
2020-12-05 16:44:19 +0530

    added hi

--- src/App.js
+++ src/App.js
@@ -7,7 +7,10 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Hello, Sukumar.
+          Hi....
+		  Hello, 
+		  Sukumar.
+		  
         </p>
         <a
           className="App-link"
